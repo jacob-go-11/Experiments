d3e74cc593a554870506e2e4dd73b6e534742ba9
Jacob Go <jacob_go@dlsu.edu.ph>
2023-02-23 10:00:20 +0800

    first_version

---
+++ guesing_game.py
@@ -0,0 +1,36 @@
+import random
+
+#generates random number from given range
+def randomNumber(lo, hi):
+    ranNum = random.randrange(lo, hi)
+    print("\nYou have 10 chances to guess the number between %d and %d." %(lo, hi))
+    return ranNum 
+
+#initial welcoming          
+print("*** Welcome to the Number Guessing Game! ***")
+print("\nYou may choose the range of numbers to guess from.")
+
+#user can choose range of numbers to guess from
+high = int(input("\nEnter maximum number: "))
+low = int(input("Enter minumum number: "))
+
+#calls randonNumber function
+num = randomNumber(low, high)
+
+numOfGuesses = 1
+#loops for ten times or until number is guessed
+while numOfGuesses < 10:
+    guess = int(input("\nEnter a number: "))
+    if guess < num:
+        print("Too low!")
+    elif guess > num:
+        print("Too high!")
+    else: 
+        break
+    numOfGuesses += 1
+
+#checks if number is guessed
+if guess == num:
+    print("Congratulations! You guessed the number %d in %d tries!" % (num, numOfGuesses))
+else:
+    print("The number is %d. Try again next time!" % num)
